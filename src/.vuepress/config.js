0b57223ac4be6bdd5437b1468efd180c276e60e9
xOnlyFadi <fadewailyosefptoo@gmail.com>
2023-01-09 19:56:40 -0700

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -13,7 +13,6 @@ module.exports = {
 		["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
 		["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
 		["meta", { name: "viewport", content: "width=device-width, initial-scale=1.0" }],
-		["meta", { name: "theme-color", content: "#D21A1C" }],
 	],
 
 	themeConfig: {
